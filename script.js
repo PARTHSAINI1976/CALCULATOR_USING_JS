fb64d1a14cae06390e3bdb818054d5e3f5c46062
ParthSaini1234 <148259804+ParthSaini1234@users.noreply.github.com>
2024-01-16 03:38:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Calculator is ready to use")
+
+let string = ""
+let button = document.querySelectorAll("#buttons");
+Array.from(button).forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        if(e.target.innerHTML == "="){
+            string=eval(string);
+            document.querySelector("input").value = string;
+         }
+         else if(e.target.innerHTML == "AC"){
+            string = ""
+            document.querySelector("input").value = string ;
+            console.log(e.target)
+         }
+         else{
+        console.log(e.target)
+        string=string + e.target.innerHTML
+        document.querySelector("input").value= string
+    }
+    })
+})
